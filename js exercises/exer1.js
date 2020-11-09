7b38fb026a8b326c3f473b724e15eb2a845fffd8
fanis-kol <72409285+fanis-kol@users.noreply.github.com>
2020-11-09 17:50:40 +0200

    Add files via upload

---
+++ js exercises/exer1.js
@@ -0,0 +1,31 @@
+function con (a, b) {
+    return(a + b);
+    }
+    document.write(con(15, 20));
+
+
+function maxNum (a, b) {
+if (a > b) {
+return a;
+}
+else if (a < b) {
+return b;
+}
+else {
+return a;
+ }
+} 
+
+document.write(maxNum(22, 15));
+
+
+function checkJava (a){
+    if (a === "java") {
+    return true;
+    }
+    else {
+    return false;
+     }
+    }
+    console.log(checkJava("java"));
+    console.log(checkJava(5));
